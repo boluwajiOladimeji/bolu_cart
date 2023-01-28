7a07a6e068bc01784f687a5b8a6f723b623064c3
eliasfreeman <oladimejiboluwaji@gmail.com>
2023-01-28 09:10:09 +0100

    new

--- index.js
+++ index.js
@@ -1,3 +1,162 @@
+// 'use strict';
+
+// import { menuArray } from './data.js';
+
+// const alertEl = document.querySelector('.alert');
+// const menuContainer = document.querySelector('.menu-container');
+// const orderContainer = document.querySelector('.order');
+// const mealOrderContainer = document.querySelector('.meal-orders');
+// const totalEl = document.querySelector('.total');
+// const cart = [];
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   displayMeal();
+//   addToCart();
+//   removeFromCart();
+// });
+
+// function addToCart() {
+//   const addBtn = document.querySelectorAll('.add-cart');
+//   addBtn.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//       for (let meal of menuArray) {
+//         if (e.currentTarget.dataset.meal === meal.name) {
+//           if (cart.includes(meal)) {
+//             alert('item already in cart', 'danger');
+//           } else {
+//             cart.push(meal);
+//             renderOrder();
+//             alert('meal added to cart', 'success');
+//           }
+//         }
+//       }
+//       //   console.log(e.currentTarget.dataset.meal);
+//     });
+//   });
+// }
+
+// function removeFromCart() {
+//   orderContainer.addEventListener('click', function (e) {
+//     for (let meal of cart) {
+//       if (e.target.dataset.meal === meal.name) {
+//         cart.pop(meal);
+
+//         if (cart.length === 0) {
+//           orderContainer.classList.remove('show-order');
+//           alert('no item in cart', 'danger');
+//         } else {
+//           renderOrder();
+//           alert('item removed from cart', 'danger');
+//         }
+
+//         // renderOrder();
+//         // alert('item removed from cart', 'danger');
+//         // if (cart.length === 0) {
+//         //   orderContainer.classList.remove('show-order');
+//         //   alert('no item in cart', 'danger');
+//         // }
+//       }
+//     }
+//   });
+// }
+
+// function displayMeal() {
+//   const displayMenu = menuArray
+//     .map(function (menu) {
+//       return ` <article class="meal">
+//               <p class="meal-emoji">${menu.emoji}</p>
+//               <div class="food-details">
+//                 <h3 class="meal-name">${menu.name}</h3>
+//                 <p class="meal-recipe">${menu.ingredients}</p>
+//                 <span class="meal-price">$${menu.price}</span>
+//               </div>
+//               <button class="btn add-cart" data-meal="${menu.name}">
+//                 <i class="fa-solid fa-plus"></i>
+//               </button>
+//             </article>`;
+//     })
+//     .join('');
+//   menuContainer.innerHTML = displayMenu;
+// }
+
+// function renderOrder() {
+//   mealOrderContainer.innerHTML = cart
+//     .map(function (meal) {
+//       return `<article class="meal-container">
+//                 <h3 class="meal-order-name">${meal.name}</h3>
+//                 <button class="remove-cart" data-meal='${meal.name}'>remove</button>
+//                 <p class="meal-order-price">$${meal.price}</p>
+//               </article>`;
+//     })
+//     .join('');
+//   orderContainer.classList.add('show-order');
+//   checkTotal();
+// }
+
+// function alert(text, outcome) {
+//   alertEl.textContent = text;
+//   alertEl.classList.add(`alert-${outcome}`);
+
+//   setTimeout(function () {
+//     alertEl.textContent = '';
+//     alertEl.classList.remove(`alert-${outcome}`);
+//   }, 1000);
+// }
+
+// function checkTotal() {
+//   let total = 0;
+
+//   cart.forEach(function (meal) {
+//     total += meal.price;
+//   });
+//   totalEl.textContent = `$${total}`;
+//   return total;
+// }
+
+// // function getTotalPrice() {
+// //   let totalPrice = checkTotal()
+// // }
+
+// // Modal
+// const orderbtn = document.querySelector('.complete-order-btn');
+// const modal = document.querySelector('.modal-container');
+// const closeModalBtn = document.querySelector('.close-modal-btn');
+// const form = document.querySelector('.form');
+// const formBtn = document.querySelector('.form-btn');
+
+// const modalEl = document.querySelector('.modal');
+// const priceValue = document.querySelector('.price-value');
+
+// orderbtn.addEventListener('click', function () {
+//   modal.classList.add('show-modal');
+//   let totalPrice = checkTotal();
+//   priceValue.textContent = `${totalPrice}.00`;
+//   formBtn.textContent = `Pay ${totalPrice}.00`;
+// });
+
+// closeModalBtn.addEventListener('click', function () {
+//   modal.classList.remove('show-modal');
+// });
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   console.log('clicked');
+//   let totalPrice = checkTotal();
+//   checkTotal();
+//   modalEl.innerHTML = `<div class="success">
+//   <div>
+//   <i class="fa-regular fa-circle-check order-success"></i>
+//   <p class="successful-pay">Payment Successful</p>
+//   <p class="success-text">You paid $${totalPrice}.00 to Bolu's dinner</p>
+//   </div>
+// </div>`;
+
+//   setTimeout(function () {
+//     alert('Order Successful', 'success');
+//     window.location.reload();
+//   }, 3000);
+// });
+
 'use strict';
 
 import { menuArray } from './data.js';
@@ -39,13 +198,16 @@ function removeFromCart() {
   orderContainer.addEventListener('click', function (e) {
     for (let meal of cart) {
       if (e.target.dataset.meal === meal.name) {
-        cart.pop(meal);
+        console.log(meal);
+        console.log(cart);
+        console.log(e.target.parentElement);
+        mealOrderContainer.removeChild(e.target.parentElement);
+        // renderOrder();
 
-        if (cart.length === 0) {
+        if (mealOrderContainer.children.length === 0) {
           orderContainer.classList.remove('show-order');
           alert('no item in cart', 'danger');
         } else {
-          renderOrder();
           alert('item removed from cart', 'danger');
         }
 
@@ -85,7 +247,6 @@ function renderOrder() {
       return `<article class="meal-container">
                 <h3 class="meal-order-name">${meal.name}</h3>
                 <button class="remove-cart" data-meal='${meal.name}'>remove</button>
-                <p>(x${meal.amount})</p>
                 <p class="meal-order-price">$${meal.price}</p>
               </article>`;
     })
@@ -106,19 +267,39 @@ function alert(text, outcome) {
 
 function checkTotal() {
   let total = 0;
-
   cart.forEach(function (meal) {
     total += meal.price;
   });
   totalEl.textContent = `$${total}`;
-  return total;
 }
 
-// function getTotalPrice() {
-//   let totalPrice = checkTotal()
-// }
-
 // Modal
+// const orderbtn = document.querySelector('.complete-order-btn');
+// const modal = document.querySelector('.modal-container');
+// const closeModalBtn = document.querySelector('.close-modal-btn');
+// const formBtn = document.querySelector('.form-btn');
+// const nameValue = document.querySelector('.name-input');
+// let name;
+
+// orderbtn.addEventListener('click', function () {
+//   modal.classList.add('show-modal');
+// });
+
+// closeModalBtn.addEventListener('click', function () {
+//   modal.classList.remove('show-modal');
+// });
+
+// formBtn.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   //   console.log(nameValue.value);
+//   name = nameValue.value;
+//   modal.classList.remove('show-modal');
+//   orderContainer.innerHTML = `<p class="notification">Thanks ${name}!, your order is on its way</p>`;
+//   alert('Order Successful', 'success');
+//   window.location.reload();
+// });
+
+// // Modal
 const orderbtn = document.querySelector('.complete-order-btn');
 const modal = document.querySelector('.modal-container');
 const closeModalBtn = document.querySelector('.close-modal-btn');
